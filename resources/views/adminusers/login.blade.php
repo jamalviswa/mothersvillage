1e7046a8a821ea281e03a2e36183f32b58156e7b
winsvts <wins.viswa@gmail.com>
2022-01-27 12:58:55 +0530

    Update login.blade.php

--- resources/views/adminusers/login.blade.php
+++ resources/views/adminusers/login.blade.php
@@ -15,11 +15,11 @@
                                     <div id="username-field" class="field-wrapper input"><svg xmlns="http://www.w3.org/200/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
                                             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                                             <circle cx="12" cy="7" r="4"></circle>
-                                        </svg><input type="text" placeholder="    Username" class="form-control" id="__BVID__11"></div>
+                                        </svg><input type="text" placeholder="Username" class="form-control" id="__BVID__11"></div>
                                     <div id="password-field" class="field-wrapper input mb-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock">
                                             <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                                             <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                                        </svg><input type="password" placeholder="    Password" class="form-control" id="__BVID__12"></div>
+                                        </svg><input type="password" placeholder="Password" class="form-control" id="__BVID__12"></div>
                                     <div class="d-sm-flex justify-content-between">
                                         <div class="field-wrapper toggle-pass d-flex align-items-center">
                                             <p class="d-inline-block">Show Password</p>
