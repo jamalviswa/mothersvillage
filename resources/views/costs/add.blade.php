4da494910badb49a44ea47504a8efbfe46651cb4
winsvts <jamal.viswatech@gmail.com>
2022-02-17 10:53:11 +0530

    Update add.blade.php

--- resources/views/costs/add.blade.php
+++ resources/views/costs/add.blade.php
@@ -7,7 +7,7 @@
         <div class="d-flex align-items-center">
             <div class="mr-auto">
                 <h3 class="m-subheader__title m-subheader__title--separator">
-                    Cost Tariff 
+                    Cost value 
                 </h3>
             </div>
             <div>
