67f82f6a03a6c93f9c1f13c984af56697f910d28
jamalviswa <98383885+jamalviswa@users.noreply.github.com>
2022-03-24 19:35:16 +0530

    Update index.blade.php

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -1,112 +1,49 @@
 @extends('layouts.admin')
 @section('content')
-<?php if ($results->count() > '0') {
-                        ?>
+
 <div class="m-grid__item m-grid__item--fluid m-wrapper">
     <!-- BEGIN: Subheader -->
     <div class="m-subheader ">
         <div class="d-flex align-items-center">
             <div class="mr-auto">
                 <h3 class="m-subheader__title m-subheader__title--separator">
-                    Customer List
+                    Payment Receipt
                 </h3>
             </div>
-            <div>
-                <a href="{{route('payments.add')}}" rel="tooltip" title="" class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--outline-2x m-btn--air m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle" data-original-title="Add Category">
-                    <i class="la la-plus"></i>
-                </a>
-            </div>
+
         </div>
     </div>
     <!-- END: Subheader -->
     <div class="m-content">
-        <div class="m-portlet">
-            <div class="m-portlet__body">
-                <!--begin::Section-->
-                <div class="m-section__content    ">
-                    <form method="GET" class="search-form form-inline " action="#">
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="s" placeholder="Search" @if(isset($_REQUEST['s'])) value="{{ $_REQUEST['s'] }}" @else value="" @endif />
-                        </div>
-                        <div class="form-group">
-                            <button class="btn btn-primary m-btn m-btn--air m-btn--custom" type="submit" name="search"><i class="fa fa-search"></i></button>
-                            <?php if (isset($_REQUEST['search'])) { ?>
-                                <a class="btn btn-danger m-btn m-btn--air m-btn--custom" href="{{route('payments.index')}}"><i class="fa fa-times"></i></a>
-                            <?php } ?>
-                        </div>
-                        <div class="btnright">
-                       
-                            <button type="button" class="btn m-1 btn-warning endbtn ">CSV</button>
-                            <button type="button" class="btn  m-1 btn-success    endbtn">Print</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+
         <div class="m-portlet">
             <div class="m-portlet__body">
                 <!--begin::Section-->
                 <div class="m-section">
                     <div class="m-section__content">
-                        <?php //if ($results->count() > '0') {
-                        ?>
-                            <div class="table-responsive">
-                                <table class="table m-table m-table--head-bg-brand">
-                                    <thead>
-                                        <tr>
-                                            <th> # </th>
-                                            <th>Name</th>
-                                            <th>Description</th>
-                                            <th class="text-center">Status</th>
-                                            <th class="text-center">Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $i = ($results->currentPage() > 1) ? $results->currentPage() * $results->perpage() : $results->currentPage();
-                                        foreach ($results as $result) {
-                                        ?>
-                                            <tr>
-                                                <td width="5%">{{ $i }}</td>
-                                                <td>{{ $result->name }}</td>
-                                                <td>{{ $result->description }}</td>
-                                                <td class="text-center">
-                                                    <a rel="tooltip" title='Update Status' href="javascript:;" data-status="<?php echo ($result['status']) ?>" data-id="<?php echo $result['customer_id']; ?>" data-toggle="modal" data-target="#update-status" class="btn btn-secondary m-btn m-btn--air m-btn--custom btn-sm btn-<?php echo $result['status'] ?>">
-                                                        {{ $result['status'] }}
-                                                    </a>
-                                                </td>
-                                                <td class="text-center">
-                                                    <div class="btn-group">
-                                                        <a rel="tooltip" class="btn btn-secondary m-btn m-btn--air m-btn--custom" title="Edit" href="{{ route("payments.edit", $result->payment_id) }}">
-                                                            <i class="fa fa-pencil"></i>
-                                                        </a>
-
-                                                        <a rel="tooltip" class="delete btn btn-secondary m-btn m-btn--air m-btn--custom" title="Delete" data-value="{{$result['payment_id']}}" href="{{ route('payments.delete',$result['payment_id']) }}">
-                                                            <i class="fa fa-trash"></i>
-                                                        </a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        <?php
-                                            $i++;
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            {!! $results->appends(\Request::except('page'))->render() !!}
-                            <!--@include('pagination.default', ['paginator' => $results])-->
-                        <?php } else { ?>
-                            <div class="text-center">
-                                <img src="{{ asset('admin/img/no-record.png') }}">
-                            </div>
-                        <?php } ?>
+                        <div>
+                            <h1>Mount Kailash Properties</h1>
+                            <p>Mothers Village <br> Nesavalar Colony Road.<br> (Before Ondipudur Flyover)<br> Singanallur, Coimbatore - 641005.<br> Ph 0422 - 4399328</p>
+                            
+                        </div>
+                        <div>Receipt</div>
+                        <div>No <span>185</span> <span>Date:...........</span></div>
+                        <div>Received with thanks from................... <span>Application No............</span></div>
+                        <div>a sum of Rupees..................</div>
+                        <div>by Cheque / DD No. ......................... <span>Dated ...............</span></div>
+                        <div>Drawn on .................. <span>Bank towards............</span></div>
+                        <div>Reffered by: .............. <span> for Mount Kailash Properties</span></div>
+                        <input type="text" disabled>
+                        <div >cheque subject to realisation <span>Authorised Signatory</span></div>
                     </div>
+
+
                 </div>
             </div>
         </div>
     </div>
-    
+</div>
+
 </div>
 <script>
     $('#update-status').on('shown.bs.modal', function(event) {
