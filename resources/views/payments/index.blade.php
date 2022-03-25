6fe610f5f9aa075f2fcc16042a9f2e362481c2d7
jamalviswa <98383885+jamalviswa@users.noreply.github.com>
2022-03-25 19:13:09 +0530

    payment

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 @section('content')
-
+<?php $requestdata = request(); ?>
 <div class="m-grid__item m-grid__item--fluid m-wrapper">
     <!-- BEGIN: Subheader -->
     <div class="m-subheader ">
@@ -10,58 +10,272 @@
                     Payment Receipt
                 </h3>
             </div>
-
+            <!-- <div>
+                <a href="{{route('customers.personal_index')}}" rel="tooltip" title="" class="m-portlet__nav-link btn btn-lg btn-secondary  
+                         m-btn m-btn--outline-2x m-btn--air m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle" data-original-title="Back to List">
+                    <i class="fa fa-long-arrow-left"></i>
+                </a>
+            </div> -->
         </div>
     </div>
     <!-- END: Subheader -->
     <div class="m-content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="m-portlet">
+                    <div class="m-portlet__body">
+                        <!--begin::Section-->
+                        <div class="m-section">
+                            <form method="post" action="{{ route('customers.personal_store') }}" id="upload" class="validation_form" enctype="multipart/form-data">
+                                @csrf
+                                <div class="col-md-12 ">
+                                    <div class="m-section__content">
+                                        <pre>
+            Mother's Village
+            Nesavalar Colony Road,
+            (Before Ondipudur flyover)
+            Singanallur, Coimbatore - 641 005
+            Ph: 0422-4599328
+        </pre>
+                                        <div class="form-group row">
+                                            <label class="col-md-1">
+                                                No <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-2">
+                                                <input value="{{ old('applicant_name') }}" type="text" style="text-transform: capitalize;" autocomplete="off" class="form-control" name="applicant_name" />
+                                                @error('applicant_name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
 
-        <div class="m-portlet">
-            <div class="m-portlet__body">
-                <!--begin::Section-->
-                <div class="m-section">
-                    <div class="m-section__content">
-                        <div>
-                            <h1>Mount Kailash Properties</h1>
-                            <p>Mothers Village <br> Nesavalar Colony Road.<br> (Before Ondipudur Flyover)<br> Singanallur, Coimbatore - 641005.<br> Ph 0422 - 4399328</p>
-                            
+                                            <label class="col-md-1 offset-md-6">
+                                                Date <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-2">
+                                                <input value="{{ old('application_number') }}" type="text" autocomplete="off" class="form-control" name="application_number" />
+                                                @error('application_number')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-5">
+                                                Received with thanks from <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-7">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-5">
+                                                Application No <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-7">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-5">
+                                                a sum of Rupees <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-7">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-5">
+                                                by Cheque/DD No <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-7">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-5">
+                                                Dated <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-7">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-5">
+                                                drawn on <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-7">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+
+                                            <label class="col-md-5">
+                                                Bank towards <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-7">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div><br>
+                                        <div class="form-group row">
+                                            <label class="col-md-1">
+                                                Referred by <span class="red">*</span>
+                                            </label>
+                                            <div class="col-md-2">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control datepicker" name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+
+                                            <h4 class="col-md-3 offset-md-6">
+                                                for Mounted Kailash Properties.
+                                            </h4>
+
+                                        </div><br>
+                                        <div class="form-group row">
+
+                                            <div class="col-md-2">
+
+                                                <input value="{{old('date_of_application') }}" autocomplete="off" type="text" class="form-control " name="date_of_application" />
+
+                                                @error('date_of_application')
+                                                <span class="invalid-feedback" role="alert">
+                                                    {{ $message }}
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label class="col-md-2">
+                                                Cheque object to realisation.
+                                            </label>
+
+
+                                            <label class="col-md-2 offset-md-7">
+                                                Authorised Signatory
+                                            </label>
+
+                                        </div>
+
+
+
+                                        <div class="form-group text-right">
+                                            <button type="submit" name="submit" class="btn btn-accent m-btn m-btn--air m-btn--custom">
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div>Receipt</div>
-                        <div>No <span>185</span> <span>Date:...........</span></div>
-                        <div>Received with thanks from................... <span>Application No............</span></div>
-                        <div>a sum of Rupees..................</div>
-                        <div>by Cheque / DD No. ......................... <span>Dated ...............</span></div>
-                        <div>Drawn on .................. <span>Bank towards............</span></div>
-                        <div>Reffered by: .............. <span> for Mount Kailash Properties</span></div>
-                        <input type="text" disabled>
-                        <div >cheque subject to realisation <span>Authorised Signatory</span></div>
                     </div>
-
-
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-</div>
-<script>
-    $('#update-status').on('shown.bs.modal', function(event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var modal = $(this)
-        modal.find('#category_id').val(button.attr('data-id'));
-        modal.find('#status').val(button.attr('data-status'));
-    });
-</script>
 <style>
-    .form-control:disabled,
-    .form-control[readonly] {
-        background-color: #F26C4F;
-        opacity: 1;
-        color: #fff;
+    .radio-sec input {
+        position: relative;
+        top: 0px;
+        margin-right: 5px;
+        margin-left: 0px;
+    }
+
+    .course-div {
+        box-shadow: 0 0 5px 2px #ddd;
+        padding: 18px;
+        margin: 15px 0;
+    }
+
+    ul {
+        list-style: none;
+        margin-left: 0px;
+    }
+
+    .btn.btn-success.btn-green {
+        background-color: green !important;
+        padding: 14px 8px !important;
+        height: 33px !important;
+        text-align: center !important;
+        margin: 8px 0 9px 6px !important;
+        color: #fff !important;
+    }
+
+    .btn.btn-success.btn-danger {
+        background-color: red !important;
+        padding: 14px 8px !important;
+        height: 33px !important;
+        text-align: center !important;
+        margin: 8px 0 9px 6px !important;
+        color: #fff !important;
+    }
+
+    a.btn.btn-danger.removebtn {
+        padding: 7px 11px;
+        height: 33px;
         text-align: center;
-        padding: 11px !important;
-        font-size: 20px;
+        margin: 8px 0 9px 6px;
+        color: #fff;
+    }
+
+    .inptwo {
+        width: 53px;
     }
 </style>
+
 @endsection
